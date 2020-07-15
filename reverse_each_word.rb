e81a9613441c356526b61d144448e7e02afdc66a
Warda Ayaz <wayaz107@gmail.com>
2020-07-15 03:19:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (sentence)
   array = sentence.split(" ")
   final_array = []
-  array.each do |sentence|
+  array.collect do |sentence|
     final_array << sentence.reverse
   end
   final_array.join(" ")
