d55e448eabad799c703937678e1799e0d737977e
Warda Ayaz <wayaz107@gmail.com>
2020-07-15 03:19:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,11 +7,11 @@ def reverse_each_word (sentence)
   final_array.join(" ")
 end
 
-def reverse_each_word_with_collect (sentence)
-  array = sentence.split (" ")
-  final_array = []
-  array.map do |sentence|
-     sentence.reverse
-  end
-  final_array.join("")
-end
+# def reverse_each_word_with_collect (sentence)
+#   array = sentence.split (" ")
+#   final_array = []
+#   array.map do |sentence|
+#      sentence.reverse
+#   end
+#   final_array.join("")
+# end
